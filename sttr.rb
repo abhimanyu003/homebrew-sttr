d2b20647f9446f339c304081d44319967846da33
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-20 06:44:21 +0530

    Brew formula update for sttr version v0.1.0

--- sttr.rb
+++ sttr.rb
@@ -11,14 +11,14 @@ class Sttr < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/abhimanyu003/sttr/releases/download/v0.1.0/sttr_0.1.0_darwin_amd64.tar.gz"
-      sha256 "06a506dc059a46c3079976e7e0f5f736aa9ac7efb4083b7287c8c79752eae49c"
+      sha256 "28eecdae01383aea8902c0d7591839486bfb55cc36c192329706366506e70cab"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/abhimanyu003/sttr/releases/download/v0.1.0/sttr_0.1.0_linux_amd64.tar.gz"
-      sha256 "8ef2505f30b589072707c5bd4606355b9e62a3bd5c7aaf3e106bf9aca410927f"
+      sha256 "1455cdcb6a7b00d6b79ed24f07551e4dd91b66bd7c362847712538b9df9ac9f7"
     end
   end
 
